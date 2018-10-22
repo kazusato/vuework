076fdaf9ea84cca9add64d06ab347c93a60bda04
Kazuhiko Sato <super80@gmail.com>
2018-10-23 06:15:07 +0900

    added MyPage.vue

---
+++ src/components/MyPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Hello World</h1>
+        <div id="msg">
+            <span class="welcomeMsg">{{message}}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MyPage',
+        data() {
+            return {
+                message: 'Welcome to the Vue world.'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
